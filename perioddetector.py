c0a54234940e2741a31a1a057355283fbe9025f1
Dan Thompson <danthompson41@gmail.com>
2015-04-13 11:57:26 -0400

    Window
    Added Comments, changed window size to accurately reflect the size of
    current micro dumps

--- perioddetector.py
+++ perioddetector.py
@@ -3,26 +3,28 @@ import matplotlib.pyplot as plt
 import csv
 import logging
 
-window = 300
+window = 100
 
 logging.basicConfig(level=logging.INFO)
 array = []
 array2 = []
-f = open('dump.csv')
+
+f = open('dump1.csv')
 csv_f = csv.reader(f)
 for row in csv_f:
     array.extend(row)
     logging.debug(row)
 logging.info(array)
+
 for i in range(len(array) - 1):
 	logging.info(array[i])
 
-
+# #Remove Outliers
 # for i in range(len(array)):
 # 	if int(array[i]) > 130:
 # 		array[i] = array[i-1]
 
-
+#Averaging
 for i in range(len(array)-10):
 	entry = (int(array[i])+int(array[i+1])+int(array[i+2])+int(array[i+3])+int(array[i+4])+int(array[i+5])+int(array[i+6])+int(array[i+7])+int(array[i+8])+int(array[i+9]))/10
 	array2.append(entry)
